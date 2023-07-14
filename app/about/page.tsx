47507e6180219f34ca85fdbb09f898a10f5dcfe9
Hengers3012 <hengersrosario.m@gmail.com>
2023-07-14 17:56:43 -0400

    feat: About Us Updated

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,12 +14,28 @@ import Link from "next/link";
 
 export default function page() {
   return (
-    <div className=" h-max">
-      <div className="mockup-window border bg-base-300">
+    <div className=" h-max w-full">
+      <div className="mockup-window border bg-base-300 -mx-4">
         <div className="flex justify-center px-4 py-16 bg-base-200 flex-col">
-          <section className="text-gray-600 body-font">
-            <div className="flex flex-col text-center w-full mb-10">
-              ABOUT US
+          <section className="text-gray-600 body-font ">
+            <div className="flex flex-col w-full items-center justify-center mb-4">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                ABOUT US
+              </h1>
+              <p className=" text-justify w-1/1 m-10 mt-4 md:w-1/1  sm:w-1/1 leading-relaxed text-base">
+                Our vision goes beyond a simple internet page, it is rather a
+                solution so that each of the users obtains what they require,
+                truthful information, products and services that meet their
+                needs and expanded with the latest technology trends. applied to
+                the internet.
+              </p>
+              <p className=" text-justify w-1/1 md:w-1/1 sm:w-1/1 m-10 mt-4 leading-relaxed text-base">
+                At Elixir Cars we are committed to offering our clients the best
+                service.
+              </p>
+              <button className="flex mx-auto mt-16 title-font font-medium text-lg text-white bg-blue-500 rounded-lg transition duration-300 hover:shadow-md shadow-[#555555] border-0 py-2 px-8 focus:outline-none hover:bg-[#FFD700] hover:text-gray-800 ">
+                More information
+              </button>
             </div>
           </section>
           <section className="text-gray-600 body-font">
@@ -483,7 +499,7 @@ export default function page() {
                       <Image
                         alt="team"
                         className=" mr-2 flex-shrink-0 h-32 w-32 bg-white rounded-lg object-cover object-top sm:mb-0 border-solid shadow-md shadow-[#555555] transition duration-500 hover:scale-105"
-                        src={person_1}
+                        src={person_5}
                         width={"500"}
                         height={"500"}
                       />
@@ -541,6 +557,52 @@ export default function page() {
                   </div>
                 </div> */}
 
+                <div className="p-4 lg:w-1/3 md:w-1/2 w-full ">
+                  <div className="h-full flex items-top border-gray-200 border p-2 rounded-lg transition duration-300 hover:shadow-md shadow-[#555555] hover:bg-[#FFD700]">
+                    <div className="flex-grow max-w-fit sm:pl-2 sm:pt-2 ">
+                      <Image
+                        alt="team"
+                        className=" mr-2 flex-shrink-0 h-32 w-32 bg-white rounded-lg object-cover object-top sm:mb-0 border-solid shadow-md shadow-[#555555] transition duration-500 hover:scale-105"
+                        src={person_6}
+                        width={"500"}
+                        height={"500"}
+                      />
+                    </div>
+                    <div className="flex-grow sm:pl-4 pl-2">
+                      <h2 className="title-font font-medium text-lg text-gray-900">
+                        Rodrigo Maximiliano Zanek
+                      </h2>
+
+                      <h3 className="text-gray-500 mb-3">Developer</h3>
+
+                      <p className="mb-4">Description:</p>
+
+                      <span className="inline-flex">
+                        <Link href={"https://github.com/"} target="black">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1em"
+                            viewBox="0 0 448 512"
+                            className="m-1 w-6 h-6"
+                          >
+                            <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z" />
+                          </svg>
+                        </Link>
+
+                        <Link href={"https://www.linkedin.com/"} target="black">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1em"
+                            viewBox="0 0 448 512"
+                            className="m-1 w-6 h-6"
+                          >
+                            <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                          </svg>
+                        </Link>
+                      </span>
+                    </div>
+                  </div>
+                </div>
                 {/* <div className="p-2 lg:w-1/3 md:w-1/2 w-full ">
                   <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg transition duration-300 hover:shadow-md hover:bg-[#FFD700]">
                     <Image
